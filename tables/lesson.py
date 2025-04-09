d8ef243bba303d31fd790cb749111b2d9a82470d
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2025-04-09 13:44:59 +0700

    Create lesson.py

---
+++ tables/lesson.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+topics = pd.read_csv('data/topics.csv')
+
+lesson_data = []
+for i in range(len(topics)):
+    lesson_data.append({
+        'lesson_id': topics['topic_id'][i],
+        'topic_id': topics['topic_id'][i],
+        'title': topics['topic_name'][i]
+    })
+
+# Create a DataFrame from the lesson data
+lesson_df = pd.DataFrame(lesson_data)
+
+# Save the DataFrame to a CSV file
+lesson_df.to_csv('data/lesson.csv', index=False)
+
+print("Lesson table has been created successfully!")
