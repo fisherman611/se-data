aa845504838e24b60f58b38131ced0829fdb4d41
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2025-04-09 11:06:00 +0700

    Update media.py

--- tables/media.py
+++ tables/media.py
@@ -21,22 +21,35 @@ def text_to_speech(text: str, output_filename: str, lang: str = "en") -> None:
 # Read sentences from CSV file
 sentences = pd.read_csv("data/sentences.csv")
 
+K = 2000
 # Convert English sentences to audio files using tqdm for progress tracking
-for i in tqdm(range(len(sentences)), desc="Converting English sentences"):
+# for i in tqdm(range(len(sentences)), desc="Converting English sentences"):
+for i in tqdm(range(0, K), desc="Converting English sentences"):
+# for i in tqdm(range(K, 2*K), desc="Converting English sentences"):
+# for i in tqdm(range(2*K, 3*K), desc="Converting English sentences"):
+# for i in tqdm(range(3*K, 4*K), desc="Converting English sentences"):
+# for i in tqdm(range(4*K, 5*K), desc="Converting English sentences"):
+# for i in tqdm(range(5*K, len(sentences)), desc="Converting English sentences"):
     text = sentences.iloc[i]["eng"]
     output_filename = f"data/media/english/eng_{i + 1}.mp3"
     try:
         text_to_speech(text, output_filename, lang="en")
     except Exception as e:
         print(f"Error processing English sentence {i + 1}: {e}")
-    time.sleep(1)  # Pause between requests
+    time.sleep(1.5)
 
 # # Convert Vietnamese sentences to audio files using tqdm for progress tracking
-# for i in tqdm(range(len(sentences)), desc="Converting Vietnamese sentences"):
+# # for i in tqdm(range(len(sentences)), desc="Converting Vietnamese sentences"):
+# for i in tqdm(range(0, K), desc="Converting Vietnamese sentences"):
+# # for i in tqdm(range(K, 2*K), desc="Converting Vietnamese sentences"):
+# # for i in tqdm(range(2*K, 3*K), desc="Converting Vietnamese sentences"):
+# # for i in tqdm(range(3*K, 4*K), desc="Converting Vietnamese sentences"):
+# # for i in tqdm(range(4*K, 5*K), desc="Converting Vietnamese sentences"):
+# # for i in tqdm(range(5*K, len(sentences)), desc="Converting Vietnamese sentences"):
 #     text = sentences.iloc[i]["viet"]
 #     output_filename = f"data/media/vietnamese/viet_{i + 1}.mp3"
 #     try:
 #         text_to_speech(text, output_filename, lang="vi")
 #     except Exception as e:
 #         print(f"Error processing Vietnamese sentence {i + 1}: {e}")
-#     time.sleep(1)  # Pause between requests
+#     time.sleep(1.5)
