3dff0c511723521546842d2b783026fed9ddf2aa
Luong Huu Thanh <117010350+fisherman611@users.noreply.github.com>
2025-06-02 14:23:05 +0700

    some updates

---
+++ tables/updated_lesson_sentence.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+import random 
+
+topics = pd.read_csv('data/topics.csv')
+sentences = pd.read_csv('data/sentences.csv')
+
+lesson_types = ['Vocab', 'Fill_in_the_blank', 'Re_order_words', 'Re_order_chars', 'Listen_and_fill']
+lesson_data = []
+for i in range(len(topics)):
+    selected_sentences = sentences[sentences['topic_name'] == topics['topic_name'][i]]['s_id'].to_list()
+    with open(f"selected_sentences_{i}.txt", "w") as f:
+        for s_id in selected_sentences:
+            f.write(f"{s_id}\n")
+#     for j in range(5):
+#         for s_id in selected_sentences[:10]:
+#             lesson_data.append({
+#                 'topic_id': topics['topic_id'][i],
+#                 'lesson_id': j + 1,
+#                 's_id': s_id
+#             })
+
+# # Create a DataFrame from the lesson data
+# lesson_df = pd.DataFrame(lesson_data)
+
+# # Save the DataFrame to a CSV file
+# lesson_df.to_csv('data/lessons_sentences.csv', index=False)
+
+print("Lesson table has been created successfully!")
